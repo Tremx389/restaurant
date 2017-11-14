4691e163ec07ecbab955cdef231ea92899a2e87e
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2017-11-14 22:05:56 +0100

    backend

---
+++ src/main/java/hu/elte/alkfejl/issuetracker/service/MenuService.java
@@ -0,0 +1,29 @@
+package hu.elte.alkfejl.issuetracker.service;
+
+import hu.elte.alkfejl.issuetracker.model.Menu;
+import hu.elte.alkfejl.issuetracker.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+    
+    @Autowired
+    private MenuRepository menuRepository;
+    
+    public Iterable<Menu> menus() {
+        return menuRepository.findAll();
+    }
+    
+    public Menu create(Menu menu) {
+        return menuRepository.save(menu);
+    }
+    
+    public Menu read(int id) {
+        return menuRepository.findOne(id);
+    }
+    
+    public void delete(int id) {
+        menuRepository.delete(id);
+    }
+}
