fd91927541731d7598135888a43cdc9c8fd743e5
Tremx389 <tremx.389@gmail.com>
2017-12-10 14:48:05 +0100

    Menu model added

---
+++ issue-tracker/src/app/model/Menu.ts
@@ -0,0 +1,9 @@
+export class City {
+  id: number;
+  menu: String;
+
+  constructor(id?: number, menu?: String) {
+    this.id = id;
+    this.menu = menu;
+  }
+}
--- issue-tracker/src/app/model/Restaurant.ts
+++ issue-tracker/src/app/model/Restaurant.ts
@@ -3,11 +3,13 @@ export class Restaurant {
   name: String;
   address: String;
   city_id: number;
+  menu_id: number;
 
-  constructor(id?: number, name?: String, address?: String, city_id?: number) {
+  constructor(id?: number, name?: String, address?: String, city_id?: number, menu_id?: number) {
     this.id = id;
     this.name = name;
     this.address = address;
     this.city_id = city_id;
+    this.menu_id = menu_id;
   }
 }
