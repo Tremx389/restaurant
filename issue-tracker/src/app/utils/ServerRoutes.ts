d7c189dbf4ff55278c73e73e1f9d5803ff513d85
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2017-12-12 19:20:03 +0100

    Server Route: cities

--- issue-tracker/src/app/utils/ServerRoutes.ts
+++ issue-tracker/src/app/utils/ServerRoutes.ts
@@ -4,16 +4,15 @@ export class Routes {
   static LOGOUT: String = 'user/logout';
   static ISSUES: String = 'issues';
   static RESTAURANTS: String = 'restaurants';
+  static CITIES: String = 'cities';
 }
 
 export class Server {
   private static address: String = 'localhost';
-  private static port: String = '4200';
+  private static port: String = '8080';
   private static prefix: String = 'api';
 
   static routeTo(route: String) {
     return `http://${Server.address}:${Server.port}/${Server.prefix}/${route}`
   }
 }
-
-
