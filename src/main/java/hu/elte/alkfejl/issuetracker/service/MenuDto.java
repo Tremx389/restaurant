3aac02a67b9e89c3fc24d0bbf2236c9453fc1015
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2017-12-16 16:39:11 +0100

    post + put menu

---
+++ src/main/java/hu/elte/alkfejl/issuetracker/service/MenuDto.java
@@ -0,0 +1,13 @@
+package hu.elte.alkfejl.issuetracker.service;
+
+import lombok.Data;
+
+
+@Data
+public class MenuDto {
+    
+    private String type;
+    private String name;
+    private int restaurantId;
+    
+}
